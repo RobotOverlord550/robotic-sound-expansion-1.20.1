eaf597cf30273725dacf898bb45943d82439d186
RobotOverlord550 <erniewhitmarsh3@gmail.com>
2026-01-07 21:46:51 -0500

    Initial commit

---
+++ src/client/java/robotoverlord550/robotic_sound_expansion/RoboticSoundExpansionClient.java
@@ -0,0 +1,10 @@
+package robotoverlord550.robotic_sound_expansion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoboticSoundExpansionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/robotoverlord550/robotic_sound_expansion/RoboticSoundExpansionDataGenerator.java
@@ -0,0 +1,11 @@
+package robotoverlord550.robotic_sound_expansion;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RoboticSoundExpansionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
